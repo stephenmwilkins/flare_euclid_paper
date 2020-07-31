9cb414fc593b31f2db393c538171a8cdb7acb30d
Stephen Wilkins <stephenwilkins@Stephens-MacBook-Pro.local>
2020-07-31 10:26:51 +0100

    added paper

---
+++ get_figures.sh
@@ -0,0 +1,7 @@
+
+
+
+cp ../analysis/basic/figures/N.pdf figures/
+cp ../analysis/pz/analysis/figures/beta_dz.pdf figures/
+cp ../analysis/pz/analysis/figures/beta_bias.pdf figures/
+cp ../analysis/pz/analysis/figures/beta_scatter.pdf figures/
